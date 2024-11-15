245c395d45c9f94defbe8986ba370a92952d53f4
samyakshankhdhar <samyak.s4u@gmail.com>
2024-11-16 01:34:19 +0530

    Add signin feature

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,5 +6,6 @@ router.post('/create-user', userController.createUser);
 router.get('/get-users', userController.getUsers);
 router.put('/update-user:id', userController.updateUser);
 router.delete('/delete-user:id', userController.deleteUser);
+router.post('/signin-user', userController.signIn);
 
 module.exports = router;
